e3585c695f812c7ff30b5743a1b0acd9f4fc8654
DaoBaoKha <daobaokha2509@gmail.com>
2025-10-03 16:49:09 +0700

    feat: add get vehicle api
    add get by id and get all api for vehicle

---
+++ SummerCampManagementSystem/SummerCampManagementSystem.BLL/Interfaces/IVehicleService.cs
@@ -0,0 +1,10 @@
+﻿using SummerCampManagementSystem.DAL.Models;
+
+namespace SummerCampManagementSystem.BLL.Interfaces
+{
+    public interface IVehicleService
+    {
+        Task<IEnumerable<Vehicle>> GetAllVehicles();
+        Task<Vehicle?> GetVehicleById(int id);
+    }
+}
---
+++ SummerCampManagementSystem/SummerCampManagementSystem.DAL/Repositories/VehicleRepository/IVehicleRepository.cs
@@ -0,0 +1,12 @@
+﻿using SummerCampManagementSystem.DAL.Models;
+using SummerCampManagementSystem.DAL.Repositories.GenericRepository;
+
+namespace SummerCampManagementSystem.DAL.Repositories.VehicleRepository
+{
+    public interface IVehicleRepository : IGenericRepository<Vehicle>
+    {
+        Task<IEnumerable<Vehicle>> GetAllVehicles();
+
+        Task<Vehicle?> GetVehicleById(int id);
+    }
+}
