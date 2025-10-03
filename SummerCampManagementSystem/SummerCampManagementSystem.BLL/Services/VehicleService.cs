1a71bff35f00d7a5e466ae69bb2f3a7688339d47
DaoBaoKha <daobaokha2509@gmail.com>
2025-10-03 16:45:16 +0700

    feat: add get vehicle api

---
+++ SummerCampManagementSystem/SummerCampManagementSystem.BLL/Services/VehicleService.cs
@@ -0,0 +1,23 @@
+﻿using SummerCampManagementSystem.BLL.Interfaces;
+using SummerCampManagementSystem.DAL.Models;
+using SummerCampManagementSystem.DAL.Repositories.VehicleRepository;
+
+namespace SummerCampManagementSystem.BLL.Services
+{
+    public class VehicleService : IVehicleService
+    {
+        private readonly IVehicleRepository _vehicleRepository;
+        public VehicleService(IVehicleRepository vehicleRepository)
+        {
+            _vehicleRepository = vehicleRepository;
+        }
+        public async Task<IEnumerable<Vehicle>> GetAllVehicles()
+        {
+            return await _vehicleRepository.GetAllVehicles();
+        }
+        public async Task<Vehicle?> GetVehicleById(int id)
+        {
+            return await _vehicleRepository.GetVehicleById(id);
+        }
+    }
+}
