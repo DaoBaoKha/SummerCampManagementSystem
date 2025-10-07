d0b6a529e64da7332754971a9229c7bb55cdf28b
DaoBaoKha <daobaokha2509@gmail.com>
2025-10-07 09:24:06 +0700

    feat: cametype crud api
    crud camptype api

---
+++ SummerCampManagementSystem/SummerCampManagementSystem.DAL/Repositories/Repository/CampTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using SummerCampManagementSystem.DAL.Models;
+using SummerCampManagementSystem.DAL.Repositories.Interfaces;
+
+namespace SummerCampManagementSystem.DAL.Repositories.Repository
+{
+    public class CampTypeRepository : GenericRepository<CampType>, ICampTypeRepository
+    {
+        public CampTypeRepository(CampEaseDatabaseContext context) : base(context)
+        {
+        }
+        public async Task<CampType?> GetCampTypeByIdAsync(int id)
+        {
+            return await _context.CampTypes.FindAsync(id);
+        }
+    }
+}
---
+++ SummerCampManagementSystem/SummerCampManagementSystem/Controllers/CampTypeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SummerCampManagementSystem.BLL.DTOs.Requests.CampType;
+using SummerCampManagementSystem.BLL.Interfaces;
+using SummerCampManagementSystem.DAL.Models;
+
+namespace SummerCampManagementSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampTypeController : ControllerBase
+    {
+        private readonly ICampTypeService _campTypeService;
+
+        public CampTypeController(ICampTypeService campTypeService)
+        {
+            _campTypeService = campTypeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCampTypes()
+        {
+            var campTypes = await _campTypeService.GetAllCampTypesAsync();
+            return Ok(campTypes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCampTypeById(int id)
+        {
+            var campType = await _campTypeService.GetCampTypeByIdAsync(id);
+            if (campType == null)
+            {
+                return NotFound(new { message = "Camp type not found" });
+            }
+            return Ok(campType);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCampType([FromBody] CampTypeRequestDto campType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var createdCampType = await _campTypeService.AddCampTypeAsync(campType);
+            return CreatedAtAction(nameof(GetCampTypeById), new { id = createdCampType.CampTypeId }, createdCampType);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCampType(int id, [FromBody] CampTypeRequestDto campType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var updatedCampType = await _campTypeService.UpdateCampTypeAsync(id, campType);
+            if (updatedCampType == null)
+            {
+                return NotFound(new { message = "Camp type not found" });
+            }
+            return Ok(updatedCampType);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCampType(int id)
+        {
+            var result = await _campTypeService.DeleteCampTypeAsync(id);
+            if (!result)
+            {
+                return NotFound(new { message = "Camp type not found" });
+            }
+            return Ok(id);
+        }
+    }
+}
