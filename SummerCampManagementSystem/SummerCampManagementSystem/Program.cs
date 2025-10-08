76009b76d56e9a66bd74c52459056989a6682b57
DaoBaoKha <daobaokha2509@gmail.com>
2025-10-09 00:06:58 +0700

    Fix2 (#60)
    * fix: email string
    
    * fix program
    
    * fix2

--- SummerCampManagementSystem/SummerCampManagementSystem/Program.cs
+++ SummerCampManagementSystem/SummerCampManagementSystem/Program.cs
@@ -106,6 +106,8 @@ builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IValidationService, ValidationService>();
 
+builder.Services.AddMemoryCache();
+
 // Email Setting
 var emailSetting = new EmailSetting
 {
