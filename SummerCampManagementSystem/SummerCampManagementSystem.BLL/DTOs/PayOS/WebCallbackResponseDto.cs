b3a419e9dd3f9bd2b8e3e39951401a694b105ff4
DaoBaoKha <daobaokha2509@gmail.com>
2025-10-14 12:05:40 +0700

    refactor: website callback api

---
+++ SummerCampManagementSystem/SummerCampManagementSystem.BLL/DTOs/PayOS/WebCallbackResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SummerCampManagementSystem.BLL.DTOs.PayOS
+{
+    public class WebCallbackResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public int OrderCode { get; set; }
+        public string Status { get; set; } = "PENDING";
+        public string Message { get; set; } = "";
+        public string Detail { get; set; } = "";
+    }
+}
