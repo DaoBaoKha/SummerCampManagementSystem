d95562f5a58f0434c868fd4b026cd92903092a58
Taisoaica <luuphuoctai2004@gmail.com>
2025-10-23 20:02:29 +0700

    feat: Create Core and Option ActivitySchedule API

--- SummerCampManagementSystem/SummerCampManagementSystem.DAL/Repositories/Interfaces/IActivityScheduleRepository.cs
+++ SummerCampManagementSystem/SummerCampManagementSystem.DAL/Repositories/Interfaces/IActivityScheduleRepository.cs
@@ -4,5 +4,7 @@ namespace SummerCampManagementSystem.DAL.Repositories.Interfaces
 {
     public interface IActivityScheduleRepository : IGenericRepository<ActivitySchedule>
     {
+        Task<bool> IsTimeOverlapAsync(int? campId, DateTime start, DateTime end);
+        Task<ActivitySchedule?> GetByIdWithActivityAsync(int id);
     }
 }
--- SummerCampManagementSystem/SummerCampManagementSystem.DAL/Repositories/Repository/ActivityScheduleRepository.cs
+++ SummerCampManagementSystem/SummerCampManagementSystem.DAL/Repositories/Repository/ActivityScheduleRepository.cs
@@ -1,12 +1,37 @@
-﻿using SummerCampManagementSystem.DAL.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SummerCampManagementSystem.DAL.Models;
 using SummerCampManagementSystem.DAL.Repositories.Interfaces;
 
 namespace SummerCampManagementSystem.DAL.Repositories.Repository
 {
     public class ActivityScheduleRepository : GenericRepository<ActivitySchedule>, IActivityScheduleRepository
     {
+        private new readonly CampEaseDatabaseContext _context;
         public ActivityScheduleRepository(CampEaseDatabaseContext context) : base(context)
         {
+            _context = context;
         }
+
+        public async Task<bool> IsTimeOverlapAsync(int? campId, DateTime start, DateTime end)
+        {
+            if (campId == null)
+                return false;
+
+            return await _context.ActivitySchedules
+            .Include(s => s.activity)
+            .AnyAsync(s =>
+                s.activity.campId == campId &&
+                !s.isOptional &&
+                s.startTime < end &&
+                s.endTime > start);
+        }
+
+        public async Task<ActivitySchedule?> GetByIdWithActivityAsync(int id)
+        {
+            return await _context.ActivitySchedules
+                .Include(s => s.activity)
+                .FirstOrDefaultAsync(s => s.activityScheduleId == id);
+        }
+
     }
 }
