7dc0ba9c22d0c5f6400007b4011d1919c242b8b9
DaoBaoKha <daobaokha2509@gmail.com>
2025-10-23 23:04:51 +0700

    refactor: update camp flow

--- SummerCampManagementSystem/SummerCampManagementSystem.BLL/DTOs/Camp/CampRequestDto.cs
+++ SummerCampManagementSystem/SummerCampManagementSystem.BLL/DTOs/Camp/CampRequestDto.cs
@@ -1,4 +1,5 @@
 ﻿using SummerCampManagementSystem.Core.Enums;
+using System; 
 
 namespace SummerCampManagementSystem.BLL.DTOs.Camp
 {
@@ -7,18 +8,22 @@ namespace SummerCampManagementSystem.BLL.DTOs.Camp
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Place { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty; 
-        public int MinParticipants { get; set; } = 0;
-        public int MaxParticipants { get; set; } = 0;
+        public string Address { get; set; } = string.Empty;
+        public int? MinParticipants { get; set; } = 0;
+        public int? MaxParticipants { get; set; } = 0;
+        public int? MinAge { get; set; }
+        public int? MaxAge { get; set; }
+
         public DateOnly? StartDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
         public DateOnly? EndDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
         public decimal? Price { get; set; } = 0;
         public string Status { get; set; } = string.Empty;
         public int? CampTypeId { get; set; }
-        public string? image { get; set; } = string.Empty;
-        public int? CreateBy { get; set; } = null;
+        public string? Image { get; set; } = string.Empty;
         public int? LocationId { get; set; } = null;
         public int? PromotionId { get; set; } = null;
+
         public DateTime? RegistrationStartDate { get; set; } = DateTime.Now;
         public DateTime? RegistrationEndDate { get; set; } = DateTime.Now;
 
@@ -28,5 +33,4 @@ namespace SummerCampManagementSystem.BLL.DTOs.Camp
     {
         public CampStatus Status { get; set; }
     }
-
 }
