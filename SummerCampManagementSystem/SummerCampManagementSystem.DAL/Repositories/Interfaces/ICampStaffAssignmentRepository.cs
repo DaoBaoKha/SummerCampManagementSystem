c6b0a85ce77efac4b500e1c05cb8d3b39b831d1c
Lưu Phước Tài <168950335+TaiPT83@users.noreply.github.com>
2025-11-12 23:09:45 +0700

    feat: GetAvailableStaffByCampId API (#167)
    * Get Campers by ParentId
    
    * refactor: Validate Create Camper API
    
    * feat: Get All Activity Schedules by StaffId
    
    * feat: Staff controller
    
    * feat: GetCampByStaff and GetScheduleByStaffAndCamp
    
    * feat: GetAvailableStaffByCampId API

--- SummerCampManagementSystem/SummerCampManagementSystem.DAL/Repositories/Interfaces/ICampStaffAssignmentRepository.cs
+++ SummerCampManagementSystem/SummerCampManagementSystem.DAL/Repositories/Interfaces/ICampStaffAssignmentRepository.cs
@@ -4,5 +4,6 @@ namespace SummerCampManagementSystem.DAL.Repositories.Interfaces
 {
     public interface ICampStaffAssignmentRepository : IGenericRepository<CampStaffAssignment>
     {
+        Task<IEnumerable<UserAccount>> GetAvailableStaffByCampIdAsync(DateTime? start, DateTime? end);
     }
 }
