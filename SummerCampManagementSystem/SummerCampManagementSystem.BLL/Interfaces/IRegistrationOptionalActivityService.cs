0b1f7b6cd348efdb3642ce94c7d638e92fb55e71
DaoBaoKha <daobaokha2509@gmail.com>
2025-11-17 12:23:46 +0700

    feat: registration optional activity api

--- SummerCampManagementSystem/SummerCampManagementSystem.BLL/Interfaces/IRegistrationOptionalActivityService.cs
+++ SummerCampManagementSystem/SummerCampManagementSystem.BLL/Interfaces/IRegistrationOptionalActivityService.cs
@@ -1,6 +1,14 @@
-﻿namespace SummerCampManagementSystem.BLL.Interfaces
+﻿using SummerCampManagementSystem.BLL.DTOs.RegistrationOptionalActivity;
+
+namespace SummerCampManagementSystem.BLL.Interfaces
 {
     public interface IRegistrationOptionalActivityService
     {
+        Task<RegistrationOptionalActivityResponseDto> GetByIdAsync(int id);
+
+        Task<IEnumerable<RegistrationOptionalActivityResponseDto>> GetAllAsync();
+
+        Task<IEnumerable<RegistrationOptionalActivityResponseDto>> SearchAsync(RegistrationOptionalActivitySearchDto searchDto);
+
     }
 }
