30021c049034b41d76b3ce5182c1945d14794e28
DaoBaoKha <daobaokha2509@gmail.com>
2025-12-02 15:31:11 +0700

    refactor: route stop return data

--- SummerCampManagementSystem/SummerCampManagementSystem.BLL/DTOs/Location/LocationResponseDto.cs
+++ SummerCampManagementSystem/SummerCampManagementSystem.BLL/DTOs/Location/LocationResponseDto.cs
@@ -20,4 +20,13 @@ namespace SummerCampManagementSystem.BLL.DTOs.Location
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty; 
     }
+
+    public class LocationDetailDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
 }
